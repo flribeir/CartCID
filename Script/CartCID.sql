c5096031e9b0d7878536118302508aaaf058c54f
Flavio Maximo <f.maximoribeiro@gmail.com>
2024-10-09 21:48:32 -0300

    Atualização do script de criação do BD

--- Script/CartCID.sql
+++ Script/CartCID.sql
@@ -2,7 +2,7 @@
 -- Servidor:                     127.0.0.1
 -- Versão do servidor:           8.0.39-0ubuntu0.20.04.1 - (Ubuntu)
 -- OS do Servidor:               Linux
--- HeidiSQL Versão:              12.8.0.6936
+-- HeidiSQL Versão:              12.6.0.6765
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -21,6 +21,7 @@ CREATE DATABASE IF NOT EXISTS `CartCID` /*!40100 DEFAULT CHARACTER SET utf8mb4 C
 USE `CartCID`;
 
 -- Copiando estrutura para tabela CartCID.Carteirinha
+DROP TABLE IF EXISTS `Carteirinha`;
 CREATE TABLE IF NOT EXISTS `Carteirinha` (
   `ID` int NOT NULL AUTO_INCREMENT,
   `CID` int NOT NULL,
@@ -56,9 +57,11 @@ CREATE TABLE IF NOT EXISTS `Carteirinha` (
   CONSTRAINT `fk_carteirinha_doencas` FOREIGN KEY (`CID`) REFERENCES `Doencas` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Exportação de dados foi desmarcado.
+-- Copiando dados para a tabela CartCID.Carteirinha: ~0 rows (aproximadamente)
+DELETE FROM `Carteirinha`;
 
 -- Copiando estrutura para tabela CartCID.Doencas
+DROP TABLE IF EXISTS `Doencas`;
 CREATE TABLE IF NOT EXISTS `Doencas` (
   `ID` int NOT NULL AUTO_INCREMENT,
   `CID` varchar(6) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
@@ -67,25 +70,44 @@ CREATE TABLE IF NOT EXISTS `Doencas` (
   PRIMARY KEY (`ID`),
   KEY `fk_doencas_grupos` (`GRUPO`),
   CONSTRAINT `fk_doencas_grupos` FOREIGN KEY (`GRUPO`) REFERENCES `Grupo_Doencas` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=99 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=108 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Exportação de dados foi desmarcado.
+-- Copiando dados para a tabela CartCID.Doencas: ~9 rows (aproximadamente)
+DELETE FROM `Doencas`;
+INSERT INTO `Doencas` (`ID`, `CID`, `Nome`, `GRUPO`) VALUES
+	(99, '6A02', 'Transtorno do Espectro do Autismo (TEA)', 1),
+	(100, '6A02.0', 'Transtorno do Espectro do Autismo sem deficiência intelectual (DI) e com comprometimento leve ou ausente da linguagem funcional', 1),
+	(101, '6A02.1', 'Transtorno do Espectro do Autismo com deficiência intelectual (DI) e com comprometimento leve ou ausente da linguagem funcional', 1),
+	(102, '6A02.2', 'Transtorno do Espectro do Autismo sem deficiência intelectual (DI) e com linguagem funcional prejudicada', 1),
+	(103, '6A02.3', 'Transtorno do Espectro do Autismo com deficiência intelectual (DI) e com linguagem funcional prejudicada', 1),
+	(104, '6A02.4', 'Transtorno do Espectro do Autismo sem deficiência intelectual (DI) e com ausência de linguagem funcional', 1),
+	(105, '6A02.5', 'Transtorno do Espectro do Autismo com deficiência intelectual (DI) e com ausência de linguagem funcional', 1),
+	(106, '6A02.Y', 'Outro Transtorno do Espectro do Autismo especificado', 1),
+	(107, '6A02.Z', 'Transtorno do Espectro do Autismo, não especificado', 1);
 
 -- Copiando estrutura para tabela CartCID.Grupo_Doencas
+DROP TABLE IF EXISTS `Grupo_Doencas`;
 CREATE TABLE IF NOT EXISTS `Grupo_Doencas` (
   `ID` int NOT NULL AUTO_INCREMENT,
   `Descrição` varchar(255) NOT NULL DEFAULT '0',
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Exportação de dados foi desmarcado.
+-- Copiando dados para a tabela CartCID.Grupo_Doencas: ~1 rows (aproximadamente)
+DELETE FROM `Grupo_Doencas`;
+INSERT INTO `Grupo_Doencas` (`ID`, `Descrição`) VALUES
+	(1, 'TEA');
 
 -- Copiando estrutura para trigger CartCID.tg_Carteirinha_I
+DROP TRIGGER IF EXISTS `tg_Carteirinha_I`;
 SET @OLDTMP_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 DELIMITER //
 CREATE TRIGGER `tg_Carteirinha_I` BEFORE INSERT ON `Carteirinha` FOR EACH ROW BEGIN
+
   SET NEW.Dt_Validade = ADDDATE(DATE(NOW()), INTERVAL 1 YEAR);
+
   SET NEW.QRCode      = (SELECT FLOOR(RAND() * 99999));
+
 END//
 DELIMITER ;
 SET SQL_MODE=@OLDTMP_SQL_MODE;
